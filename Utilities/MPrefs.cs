f293d9dfb6fb909189ae89034a5a0d2308f03ef2
Martin Halldin <94115326+FGH21marha@users.noreply.github.com>
2023-07-18 23:52:18 +0200

    Update MPrefs.cs

--- Utilities/MPrefs.cs
+++ Utilities/MPrefs.cs
@@ -126,5 +126,163 @@ public static class MPrefs
     }
     #endregion
 
+    #region Vector2
+    public static void SetVector2( string key , Vector2 value )
+    {
+        TryCreateFile( key );
+
+        var jSon = JsonUtility.ToJson( value );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static Vector2 GetVector2( string key )
+    {
+        if (!HasKey( key ))
+            return Vector2.zero;
+
+        var data = File.ReadAllText( AsPath( key ) );
+
+        return JsonUtility.FromJson<Vector2>( data );
+    }
+    #endregion
+
+    #region Vector3
+    public static void SetVector3( string key , Vector3 value )
+    {
+        TryCreateFile( key );
+
+        var jSon = JsonUtility.ToJson( value );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static Vector3 GetVector3( string key )
+    {
+        if (!HasKey( key ))
+            return Vector3.zero;
+
+        var data = File.ReadAllText( AsPath( key ) );
+
+        return JsonUtility.FromJson<Vector3>( data );
+    }
+    #endregion
+
+    #region Vector4
+    public static void SetVector4( string key , Vector4 value )
+    {
+        TryCreateFile( key );
+
+        var jSon = JsonUtility.ToJson( value );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static Vector4 GetVector4( string key )
+    {
+        if (!HasKey( key ))
+            return Vector4.zero;
+
+        var data = File.ReadAllText( AsPath( key ) );
+
+        return JsonUtility.FromJson<Vector4>( data );
+    }
+    #endregion
+
+    #region Color
+    public static void SetColor( string key , Color value )
+    {
+        TryCreateFile( key );
+
+        var jSon = JsonUtility.ToJson( value );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static Color GetColor( string key )
+    {
+        if (!HasKey( key ))
+            return Color.white;
+
+        var data = File.ReadAllText( AsPath( key ) );
+
+        return JsonUtility.FromJson<Color>( data );
+    }
+    #endregion
+
+    #region Quaternion
+    public static void SetQuaternion( string key , Quaternion value )
+    {
+        TryCreateFile( key );
+
+        var jSon = JsonUtility.ToJson( value );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static Quaternion GetQuaternion( string key )
+    {
+        if (!HasKey( key ))
+            return Quaternion.identity;
+
+        var data = File.ReadAllText( AsPath( key ) );
+
+        return JsonUtility.FromJson<Quaternion>( data );
+    }
+    #endregion
+
+    #region Transform
+    [System.Serializable] private class MPrefsTransform
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+    }
+    public static void SetTransform( string key , Transform value )
+    {
+        TryCreateFile( key );
+
+        MPrefsTransform t = new MPrefsTransform
+        {
+            position = value.position ,
+            rotation = value.rotation ,
+            scale = value.localScale
+        };
+
+        var jSon = JsonUtility.ToJson( t );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static Transform GetTransform(this Transform transform , string key )
+    {
+        if (!HasKey( key ))
+            return null;
+
+        var data = File.ReadAllText( AsPath( key ) );
+        var t = JsonUtility.FromJson<MPrefsTransform>( data );
+
+        transform.position = t.position;
+        transform.rotation = t.rotation;
+        transform.localScale = t.scale;
+
+        return transform;
+    }
+    #endregion
+
+    #region Object
+    public static void SetObject( string key , object value )
+    {
+        TryCreateFile( key );
+
+        var jSon = JsonUtility.ToJson( value );
+
+        File.WriteAllText( AsPath( key ) , jSon );
+    }
+    public static T GetObject<T>( string key )
+    {
+        if (!HasKey( key ))
+            return default(T);
+
+        var data = File.ReadAllText( AsPath( key ) );
+
+        return JsonUtility.FromJson<T>( data );
+    }
+    #endregion
+
     #endregion
 }
